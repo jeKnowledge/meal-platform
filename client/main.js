483ff7bed88537d0d139725692678570196d0a56
David Gomes <davidrafagomes@gmail.com>
2014-05-07 14:47:39 +0100

    Removes some unnecessary code from main.js.

--- client/main.js
+++ client/main.js
@@ -1,15 +1,1 @@
-Template.main.rendered = function() { };
-
-Template.main.events({
-
-});
-
-Meteor.startup(function() {
-  //Meteor.subscribe('meals');
-});
-
-Deps.autorun(function() {
-  //Meteor.subscribe('meals');
-});
-
 Meteor.subscribe('meals');
